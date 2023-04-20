75e5891c20803ea51b1794762becf2414348bb27
eyass445 <eyass445@gmail.com>
2023-04-20 06:45:14 +0300

    commit situp

---
+++ contracts/IUniswapV3PoolCreator.sol
@@ -0,0 +1,82 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "./IWETH.sol";
+
+
+interface IUniswapV3PoolCreator {
+    // function onERC721Received(
+    //     address operator,
+    //     address from,
+    //     uint tokenId,
+    //     bytes calldata data
+    // ) external returns (bytes4);
+
+    function swap(
+        address _tokenIn,
+        address _tokenOut,
+        uint256 _amountIn,
+        uint256 _amountOutMin,
+        uint24 _fee
+    ) external;
+
+    function unwrapWETH(address wtoken, uint256 amount) external;
+
+    function wrapETH(address wtoken, uint256 amount) external;
+
+    function transferAndApprove(
+        IERC20 tokenA,
+        IWETH tokenB,
+        uint amount0ToAdd,
+        uint amount1ToAdd
+    ) external;
+
+    function createPoolAndMintNFT(
+        address token0,
+        address token1,
+        uint24 fee,
+        uint256 amount0ToAdd,
+        uint256 amount1ToAdd,
+        uint256 initialPrice,
+        uint160 minPrice,
+        uint160 maxPrice
+    ) external payable returns (address pool, int24 _tickLower, int24 _tickUpper, uint tokenId, uint128 liquidity, uint amount0, uint amount1);
+
+    function createAndInitializePool(
+        address token0,
+        address token1,
+        uint24 fee,
+        uint256 initialPrice
+    ) external payable returns (address pool);
+
+    function collectAllFees(
+        uint tokenId
+    ) external returns (uint amount0, uint amount1);
+
+    function increaseLiquidityCurrentRange(
+        address token0,
+        address token1,
+        uint tokenId,
+        uint amount0ToAdd,
+        uint amount1ToAdd
+    ) external returns (uint128 liquidity, uint amount0, uint amount1);
+
+    function decreaseLiquidityCurrentRange(
+        uint tokenId,
+        uint128 liquidity
+    ) external returns (uint amount0, uint amount1);
+
+    function removePool(
+        uint tokenId,
+        uint128 liquidity
+    ) external returns (uint amount0, uint amount1, uint fees0, uint fees1);
+
+    function transferERC20(
+        address tokenContractAddress,
+        address recipient,
+        uint256 amount
+    ) external;
+
+    function sendEther(address payable recipient, uint256 amount) external;
+}
---
+++ contracts/UniswapV3NFTPoolCreator.sol
@@ -0,0 +1,308 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@uniswap/v3-periphery/contracts/interfaces/ISwapRouter.sol";
+import "@uniswap/v3-core/contracts/interfaces/IUniswapV3Factory.sol";
+import "@uniswap/v3-core/contracts/interfaces/IUniswapV3Pool.sol";
+import "https://github.com/Uniswap/v3-periphery/blob/0.8/contracts/interfaces/INonfungiblePositionManager.sol";
+import "@uniswap/v3-periphery/contracts/interfaces/IMulticall.sol";
+import "https://github.com/Uniswap/v3-core/blob/main/contracts/libraries/TransferHelper.sol";
+import "@uniswap/v3-core/contracts/libraries/FixedPoint96.sol";
+import "./UniswapV3PriceCalculator.sol";
+import "./IWETH.sol";
+import "./IUniswapV3PoolCreator.sol";
+
+
+// IERC721Receiver ,
+contract UniswapV3PoolCreator is IUniswapV3PoolCreator  {
+    // Uniswap V3 Factory address
+    address public constant UNISWAP_V3_FACTORY = 0x1F98431c8aD98523631AE4a59f267346ea31F984;
+
+    // Uniswap V3 NFT Position Manager address
+    address public constant UNISWAP_V3_NFT_POSITION_MANAGER = 0xC36442b4a4522E871399CD717aBDD847Ab11FE88;
+
+    address private constant UNISWAP_V3_ROUTER = 0xE592427A0AEce92De3Edee1F18E0157C05861564;
+
+
+    
+    int24 private constant MIN_TICK = -887272;
+    int24 private constant MAX_TICK = -MIN_TICK;
+    int24 private constant TICK_SPACING = 60;
+
+
+    IUniswapV3Factory private v3Factory;
+    INonfungiblePositionManager private nftPositionManager;
+    ISwapRouter private swapRouter;
+
+
+    constructor() {
+        v3Factory = IUniswapV3Factory(UNISWAP_V3_FACTORY);
+        nftPositionManager = INonfungiblePositionManager(UNISWAP_V3_NFT_POSITION_MANAGER);
+        swapRouter = ISwapRouter(UNISWAP_V3_ROUTER);
+
+    }
+    receive() external payable {}
+
+   
+
+    function swap(
+        address _tokenIn,
+        address _tokenOut,
+        uint256 _amountIn,
+        uint256 _amountOutMin,
+        uint24 _fee
+    ) external {
+        //require(IERC20(_tokenIn).transferFrom(msg.sender, address(this), _amountIn), "Transfer failed");
+        require(IERC20(_tokenIn).approve(UNISWAP_V3_ROUTER, _amountIn), "Approval failed");
+ 
+
+        ISwapRouter.ExactInputSingleParams memory params = ISwapRouter.ExactInputSingleParams({
+            tokenIn: _tokenIn,
+            tokenOut: _tokenOut,
+            fee: _fee, // Choose an appropriate fee tier (e.g., 500, 3000, or 10000)
+            recipient: address(this),
+            deadline: block.timestamp + 3600, // Set an appropriate deadline
+            amountIn: _amountIn,
+            amountOutMinimum: _amountOutMin,
+            sqrtPriceLimitX96: 0 // No price limit
+        });
+
+        swapRouter.exactInputSingle(params);
+    }
+
+     // Unwraps WETH to ETH
+     function unwrapWETH(address wtoken ,uint256 amount) public {
+        require(amount > 0, "Amount must be greater than 0");
+
+        // Unwrap WETH to ETH
+        IWETH(wtoken).withdraw(amount);
+
+        // Transfer unwrapped ETH back to the user
+        (bool success, ) = address(this).call{value: amount}("");
+        require(success, "Failed to transfer Ether");
+    }
+
+    
+    // Function to wrap a specific amount of ETH held by the contract to WETH
+    function wrapETH(address wtoken, uint256 amount) external {
+        uint256 contractEthBalance = address(this).balance;
+        require(contractEthBalance >= amount, "Insufficient ETH balance in the contract");
+        require(amount > 0, "Amount must be greater than 0");
+
+        // Wrap the specified amount of ETH held by the contract to WETH
+        IWETH(wtoken).deposit{value: amount}();
+    }
+
+   
+
+
+    function transferAndApprove(
+        IERC20 tokenA,
+        IWETH tokenB,
+        uint amount0ToAdd,
+        uint amount1ToAdd
+    ) external  {
+        //IERC20 tokenA = IERC20(_tokenA);
+        //IWETH  tokenB = IWETH(_tokenB);
+
+        tokenA.transferFrom(msg.sender, address(this), amount0ToAdd);
+        tokenB.transferFrom(msg.sender, address(this), amount1ToAdd);
+
+        tokenA.approve(address(nftPositionManager), amount0ToAdd);
+        tokenB.approve(address(nftPositionManager), amount1ToAdd);
+    }
+
+  
+   
+    function createPoolAndMintNFT(address token0,
+        address token1,
+        uint24 fee,
+        uint256 amount0ToAdd,
+        uint256 amount1ToAdd,
+        //address recipient,
+        uint256 initialPrice,
+        uint160 minPrice,
+        uint160 maxPrice
+        ) external  payable returns (address pool ,int24 _tickLower ,int24 _tickUpper , uint tokenId, uint128 liquidity, uint amount0, uint amount1) {
+
+        pool = this.createAndInitializePool{value: msg.value}(token0, token1, fee, initialPrice);
+
+        IERC20 tokenA = IERC20(token0);
+        IWETH  tokenB = IWETH(token1);
+
+
+        tokenA.transferFrom(msg.sender, address(this), amount0ToAdd);
+        tokenB.transferFrom(msg.sender, address(this), amount1ToAdd);
+
+        tokenA.approve(address(nftPositionManager), amount0ToAdd);
+        tokenB.approve(address(nftPositionManager), amount1ToAdd);
+
+
+        ( _tickLower , _tickUpper ) = UniswapV3PriceCalculator.calculateTicksFromPriceRange(minPrice , maxPrice);
+
+
+        INonfungiblePositionManager.MintParams memory mintParams = INonfungiblePositionManager.MintParams({
+            token0: token0,
+            token1: token1,
+            fee: fee,
+            tickLower: _tickLower,
+            tickUpper: _tickUpper,
+            amount0Desired: amount0ToAdd,
+            amount1Desired: amount1ToAdd,
+            amount0Min: 0,
+            amount1Min: 0,
+            recipient: address(this),
+            deadline: block.timestamp
+        });
+
+
+        (tokenId, liquidity, amount0, amount1) = nftPositionManager.mint(
+            mintParams
+        );
+
+        if (amount0 < amount0ToAdd) {
+            tokenA.approve(address(nftPositionManager), 0);
+            uint refund0 = amount0ToAdd - amount0;
+            tokenA.transfer(msg.sender, refund0);
+        }
+        if (amount1 < amount1ToAdd) {
+            tokenB.approve(address(nftPositionManager), 0);
+            uint refund1 = amount1ToAdd - amount1;
+            tokenB.transfer(msg.sender, refund1);
+        }
+    }
+
+    
+    function createAndInitializePool(
+        address token0,
+        address token1,
+        uint24 fee,
+        uint256 initialPrice
+    ) external payable returns (address pool) {
+        
+        //uint160 x  = initialPrice * (2 << 96);
+        uint160 sqrtPriceX96 = UniswapV3PriceCalculator.calculateSqrtPriceX96(initialPrice);
+        address newPool = nftPositionManager.createAndInitializePoolIfNecessary{value: msg.value}(
+            token0,
+            token1,
+            fee,
+            sqrtPriceX96
+        );
+
+        return newPool;
+        // Perform additional operations with the created or initialized pool if necessary
+    }
+
+
+    function collectAllFees(
+        uint tokenId
+    ) external returns (uint amount0, uint amount1) {
+        INonfungiblePositionManager.CollectParams
+            memory params = INonfungiblePositionManager.CollectParams({
+                tokenId: tokenId,
+                recipient: address(this),
+                amount0Max: type(uint128).max,
+                amount1Max: type(uint128).max
+            });
+
+        (amount0, amount1) = nftPositionManager.collect(params);
+    }
+
+    function increaseLiquidityCurrentRange(
+        address token0,
+        address token1,
+        uint tokenId,
+        uint amount0ToAdd,
+        uint amount1ToAdd
+    ) external returns (uint128 liquidity, uint amount0, uint amount1) {
+
+        IERC20 tokenA = IERC20(token0);
+        IWETH  tokenB = IWETH(token1);
+
+        tokenA.transferFrom(msg.sender, address(this), amount0ToAdd);
+        tokenB.transferFrom(msg.sender, address(this), amount1ToAdd);
+
+        tokenA.approve(address(nftPositionManager), amount0ToAdd);
+        tokenB.approve(address(nftPositionManager), amount1ToAdd);
+
+        INonfungiblePositionManager.IncreaseLiquidityParams memory params = INonfungiblePositionManager.IncreaseLiquidityParams({
+                tokenId: tokenId,
+                amount0Desired: amount0ToAdd,
+                amount1Desired: amount1ToAdd,
+                amount0Min: 0,
+                amount1Min: 0,
+                deadline: block.timestamp
+            });
+
+        (liquidity, amount0, amount1) = nftPositionManager.increaseLiquidity(
+            params
+        );
+    }
+
+    function decreaseLiquidityCurrentRange(
+        uint tokenId,
+        uint128 liquidity
+    ) external returns (uint amount0, uint amount1) {
+        INonfungiblePositionManager.DecreaseLiquidityParams
+            memory params = INonfungiblePositionManager.DecreaseLiquidityParams({
+                tokenId: tokenId,
+                liquidity: liquidity,
+                amount0Min: 0,
+                amount1Min: 0,
+                deadline: block.timestamp
+            });
+
+        (amount0, amount1) = nftPositionManager.decreaseLiquidity(params);
+    }
+
+    function removePool(uint tokenId, uint128 liquidity) external returns (uint amount0, uint amount1, uint fees0, uint fees1) {
+        // Decrease liquidity
+        INonfungiblePositionManager.DecreaseLiquidityParams
+            memory decreaseLiquidityParams = INonfungiblePositionManager.DecreaseLiquidityParams({
+                tokenId: tokenId,
+                liquidity: liquidity,
+                amount0Min: 0,
+                amount1Min: 0,
+                deadline: block.timestamp
+            });
+
+        (amount0, amount1) = nftPositionManager.decreaseLiquidity(decreaseLiquidityParams);
+
+        // Collect fees
+        INonfungiblePositionManager.CollectParams
+            memory collectParams = INonfungiblePositionManager.CollectParams({
+                tokenId: tokenId,
+                recipient: address(this),
+                amount0Max: type(uint128).max,
+                amount1Max: type(uint128).max
+            });
+
+        (fees0, fees1) = nftPositionManager.collect(collectParams);
+    }
+
+    function transferERC20(
+        address tokenContractAddress,
+        address recipient,
+        uint256 amount
+    ) external {
+        IERC20 token = IERC20(tokenContractAddress);
+        require(token.balanceOf(address(this)) >= amount, "Insufficient balance");
+
+        // In a real-world scenario, you would need to implement private key handling and signing.
+        // This example assumes the sender has granted approval to the contract for transferring tokens.
+        token.transfer(recipient, amount);
+    }
+
+    // Function to send Ether from the contract to a recipient
+    function sendEther(address payable recipient, uint256 amount) external {
+        require(address(this).balance >= amount, "Insufficient balance in the contract");
+
+        // Transfer Ether to the recipient
+        recipient.transfer(amount);
+    }
+
+
+
+}
